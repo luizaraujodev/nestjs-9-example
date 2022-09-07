51c8ae0a1b1151f02078db7fe7928e0afd53b068
Luiz Araújo <luizhbd@gmail.com>
2022-09-07 11:04:30 -0300

    first commit

---
+++ src/bank-accounts/bank-accounts.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { BankAccountsService } from './bank-accounts.service';
+import { CreateBankAccountDto } from './dto/create-bank-account.dto';
+import { TransferBankAccountDto } from './dto/transfer-bank-account.dto';
+
+@Controller('bank-accounts')
+export class BankAccountsController {
+  constructor(private readonly bankAccountsService: BankAccountsService) {}
+
+  @Post()
+  create(@Body() createBankAccountDto: CreateBankAccountDto) {
+    return this.bankAccountsService.create(createBankAccountDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.bankAccountsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.bankAccountsService.findOne(id);
+  }
+
+  @HttpCode(204)
+  @Post('transfer')
+  async transfer(@Body() transferDto: TransferBankAccountDto) {
+    await this.bankAccountsService.transfer(
+      transferDto.from,
+      transferDto.to,
+      transferDto.amount,
+    );
+  }
+}
